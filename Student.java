51f1c2126ea8b8cd29ea99b2a35b18c69a799eb0
VaideviShetty <110751028+VaideviShetty@users.noreply.github.com>
2022-09-03 22:23:34 +0530

    Add files via upload
    day1

---
+++ Student.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Student {
+	
+	
+	String studentName= "vaidevi";
+	int rollNo= 112;
+	String collegeName= "JNTUK";
+    int markScored= 956;
+    float ccpa= 9.5F;
+	
+    
+    
+	
+
+}
