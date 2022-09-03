c63cdcca13c3a305a54dec63c91d1269c9b6f341
VaideviShetty <110751028+VaideviShetty@users.noreply.github.com>
2022-09-03 21:07:24 +0530

    Add files via upload
    day1

--- Browser.java
+++ Browser.java
@@ -3,8 +3,9 @@ package week1.day1;
 public class Browser {
 
 	public static void main(String[] args) {
-	
-System.out.println("this is my browser");
+	Chrome obj=new Chrome();
+	obj.getClass();
+	obj.PrintName();
 	
 	}
 
